6f755d78135ec362779bf2b96487e7b05c9f58bd
helosantosdesousa <ssheloly@gmail.com>
2025-04-09 13:13:14 -0300

    fim da aula 01

--- src/main/java/br/com/helosantosdesousa/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/helosantosdesousa/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,19 @@
 package br.com.helosantosdesousa.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	//como se fosse o metodo main
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello world");
+	}
 }
